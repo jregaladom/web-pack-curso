3e435d86d2119bae4090d8163016d22446742e3e
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-05-04 14:22:27 -0600

    Avance curso

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const DotEnv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -13,6 +11,8 @@ module.exports = {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
+    mode: 'development',
+    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -82,13 +82,5 @@ module.exports = {
             ]
         }),
         new DotEnv(),
-        new CleanWebpackPlugin(),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ]
-    }
 }
